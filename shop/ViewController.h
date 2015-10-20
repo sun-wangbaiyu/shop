38d22f7f7fd6407942258ff712a792e6959e54de
sun-wangbaiyu <844435887@qq.com>
2015-10-20 12:31:43 +0800

    Initial Commit

---
+++ shop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shop
+//
+//  Created by 王柏宇 on 15/10/20.
+//  Copyright © 2015年 王柏宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ shop/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  shop
+//
+//  Created by 王柏宇 on 15/10/20.
+//  Copyright © 2015年 王柏宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
